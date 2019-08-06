db534ae128194df018a05e93e7e84637846a3a7d
Cryptopurr Columbus <cryptopurrcolumbus@gmail.com>
2019-08-06 23:08:49 +0530

    Update code for app.js, make it more professional
    Signed-off-by: Cryptopurr Columbus <cryptopurrcolumbus@gmail.com>

--- js/app.js
+++ js/app.js
@@ -1,118 +1,18 @@
 // Initiate a few variables
-let openedcards = [];
-let matchedcards = [];
-let clickcount = 0;
+let openCards = [];
+let matchedCards = [];
+let clickCount = 0;
 let timeCount = 0;
 let timerPtr;
 
-// Funcation which adds the timer feature
+// Function which adds the timer feature
 function startTimer() {
   timeCount += 1;
   timerPtr = setTimeout(startTimer, 1000);
 }
 
-// shuffle the cards
-function implementshuffle() {
-  let array = [...document.getElementsByClassName('card')];
-  let woodoo = document.getElementsByClassName('deck')[0];
-  while (woodoo.firstChild) {
-    woodoo.removeChild(woodoo.firstChild);
-  }
-  array = shuffle(array);
-  for (element of array) {
-    woodoo.appendChild(element);
-  }
-}
-
-function updatemove() {
-  document.getElementsByClassName('moves')[0].textContent = clickcount;
-}
-
-function clickelement(e) {
-  if (e.target.nodeName === 'LI') {
-    clickcount++;
-    updatemove();
-    istar();
-    let a = e.target;
-    openedcards.push(a);
-    a.classList.add('open', 'show');
-    if (openedcards.length == 2) {
-      let x = openedcards[0].getElementsByClassName('fa')[0];
-      let y = openedcards[1].getElementsByClassName('fa')[0];
-      if (x.className == y.className) {
-        g = x.closest('li');
-        h = y.closest('li');
-        g.classList.add('match');
-        h.classList.add('match');
-        matchedcards.push(openedcards[0]);
-        matchedcards.push(openedcards[1]);
-      } else {
-        // css or js for showing open show class is not
-        // working here
-        g = x.closest('li');
-        h = y.closest('li');
-        g.classList.remove('open', 'show');
-        h.classList.remove('open', 'show');
-      }
-      openedcards = [];
-    }
-  } else {
-    console.log('No card was clicked. Please click a card.');
-  }
-}
-
-function youwin(e) {
-  if (matchedcards.length == 16) {
-    alert(`Congratulations! You have won! You took ${timeCount} 
-			seconds to complete the game.`);
-  }
-}
-
-function resetdeck() {
-  let carddecks = [...document.getElementsByClassName('card')];
-  for (carddeck of carddecks) {
-    carddeck.classList.remove('open', 'show', 'match');
-  }
-
-  implementshuffle();
-
-  let ul = document.getElementsByClassName('stars')[0];
-  ul.children[2].style.display = 'inline-block';
-  ul.children[1].style.display = 'inline-block';
-
-  clickcount = 0;
-  updatemove();
-
-  timeCount = 0;
-  clearTimeout(timerPtr);
-  startTimer();
-
-  matchedcards = [];
-}
-
-function removestar2() {
-  let ul = document.getElementsByClassName('stars')[0];
-  if (ul.children.length == 3) {
-    ul.children[2].style.display = 'none';
-  }
-}
-
-function removestar1() {
-  let ul = document.getElementsByClassName('stars')[0];
-  if (ul.children.length == 2) {
-    ul.children[1].style.display = 'none';
-  }
-}
-
-function istar(e) {
-  if (clickcount > 16 && clickcount < 28) {
-    removestar2();
-  } else if (clickcount >= 28) {
-    removestar1();
-  }
-}
-
-function shuffle(array) {
+// Helper function to shuffle the cards
+function shuffleArray(array) {
   var currentIndex = array.length,
     temporaryValue,
     randomIndex;
@@ -128,12 +28,127 @@ function shuffle(array) {
   return array;
 }
 
-const deckSel = document.getElementsByClassName('deck')[0];
-deckSel.addEventListener('click', clickelement);
-deckSel.addEventListener('click', youwin);
+// Function to shuffle Deck
+// Remove all cards from DOM,
+// use the shuffleArray method,
+// Reinsert in the DOM
+function shuffleDeck() {
+  let cards = [...document.getElementsByClassName('card')];
+  let deck = document.getElementsByClassName('deck')[0];
+  while (deck.firstChild) {
+    deck.removeChild(deck.firstChild);
+  }
+  cards = shuffleArray(cards);
+  for (element of cards) {
+    deck.appendChild(element);
+  }
+}
 
-const restartbtn = document.getElementsByClassName('restart')[0];
-restartbtn.addEventListener('click', resetdeck);
+function updateMove() {
+  document.getElementsByClassName('moves')[0].textContent = clickCount;
+}
 
-document.addEventListener('DOMContentLoaded', implementshuffle);
-startTimer();
+function checkAndUpdateStar(e) {
+  let ul = document.getElementsByClassName('stars')[0];
+  if (clickCount > 16 && clickCount < 28) {
+    if (ul.children.length == 3) {
+      ul.children[2].style.display = 'none';
+    }
+  } else if (clickCount >= 28) {
+    if (ul.children.length == 2) {
+      ul.children[1].style.display = 'none';
+    }
+  }
+}
+
+// When the click count increases, update the moves count
+// in DOM, check if star count needs adjustment
+function cardClickCountHandler() {
+    clickCount++;
+    updateMove();
+    checkAndUpdateStar();
+}
+
+function cardOnClickHandler(e) {
+  let eventTarget = e.target;
+  // We only consider clicks on the cards
+  if (eventTarget.nodeName === 'LI') {
+    cardClickCountHandler();
+    openCards.push(eventTarget);
+    // Open the cards
+    eventTarget.classList.add('open', 'show');
+    // We keep the max length of openCards to 2
+    // and thus we don't need to do anything
+    // when there is only one element in openCards
+    // apart from keeping the one element open
+    if (openCards.length == 2) {
+      let prev = openCards[0].getElementsByClassName('fa')[0];
+      let curr = openCards[1].getElementsByClassName('fa')[0];
+      prevCard = prev.closest('li');
+      currCard = curr.closest('li');
+      if (prev.className == curr.className) {
+        // The cards matched
+        prevCard.classList.add('match');
+        currCard.classList.add('match');
+        matchedCards.push(openCards[0]);
+        matchedCards.push(openCards[1]);
+      } else {
+        // The cards didn't match
+        prevCard.classList.remove('open', 'show');
+        currCard.classList.remove('open', 'show');
+      }
+      // The max length of openCards we want is 2.
+      openCards = [];
+    }
+  } else {
+    console.log('No card was clicked. Please click a card.');
+  }
+}
+
+// check if victory condition has been met
+// if so, alert the user.
+function victoryHandler(e) {
+  if (matchedCards.length == 16) {
+    alert(`Congratulations! You have won! You took ${timeCount} 
+			seconds to complete the game.`);
+  }
+}
+
+// Reset the whole game platform, we want to play again
+function resetDeck() {
+  let cards = [...document.getElementsByClassName('card')];
+  for (card of cards) {
+    card.classList.remove('open', 'show', 'match');
+  }
+
+  shuffleDeck();
+
+  let ul = document.getElementsByClassName('stars')[0];
+  ul.children[2].style.display = 'inline-block';
+  ul.children[1].style.display = 'inline-block';
+
+  clickCount = 0;
+  updateMove();
+
+  timeCount = 0;
+  clearTimeout(timerPtr);
+  startTimer();
+
+  matchedCards = [];
+}
+
+
+// Register event handlers, start the timer
+function main() {
+  const deckSel = document.getElementsByClassName('deck')[0];
+  deckSel.addEventListener('click', cardOnClickHandler);
+  deckSel.addEventListener('click', victoryHandler);
+
+  const restartBtn = document.getElementsByClassName('restart')[0];
+  restartBtn.addEventListener('click', resetDeck);
+
+  document.addEventListener('DOMContentLoaded', shuffleDeck);
+  startTimer();
+}
+
+main();
